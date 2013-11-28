666d4061bfcdc86195900e62a46975222aa0dc7a
Vanessa <v.lauren.lee@gmail.com>
2013-11-28 00:08:03 -0500

    added reply to sms

--- run.py
+++ run.py
@@ -1,8 +1,31 @@
 from twilio.rest import TwilioRestClient
-from flask import flask
+import twilio.twiml
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+ 
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/receive-sms", methods=['GET', 'POST'])
+def reply():
+	print "Received Message"
+	if request.form['Body'] 
+	resp = twilio.twiml.Response()
+	resp.message("<Next Question>")
+	return str(resp)
+
+else:
 
  
-# Your Account Sid and Auth Token from twilio.com/user/account
+if __name__ == "__main__":
+    app.run(debug=True)
+
+ 
+''' Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = "ACf22848a320c719539086936ae83b210c"
 auth_token  = "bb09e98f22a93cd446056680610e5078"
 client = TwilioRestClient(account_sid, auth_token)
@@ -10,7 +33,7 @@ client = TwilioRestClient(account_sid, auth_token)
 message = client.sms.messages.create(body="Hi Justine! Do you need help?",
     to="+14167104589",    # Replace with your phone number
     from_="+16132090604") # Replace with your Twilio number
-print message.sid
+print message.sid'''
 
 
 
